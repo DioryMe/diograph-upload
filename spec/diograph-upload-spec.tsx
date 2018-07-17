6281e4179bcb547cbcdb269c6462ebab87a7194a
Jouni Alanen <jouni@diory.me>
2018-07-17 21:57:50 +0300

    First spec for DiographUpload

---
+++ spec/diograph-upload-spec.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import { configure, shallow } from 'enzyme'
+import * as Adapter from 'enzyme-adapter-react-16';
+import { DiographUpload } from '../app/diograph-upload'
+
+describe('<DiographUpload />', () => {
+  let wrapper, component, secrets
+  configure({ adapter: new Adapter() })
+
+  beforeEach(() => {
+    secrets = {"master": "kissa123"}
+    wrapper = shallow(
+      <DiographUpload secrets={ secrets } />
+    )
+    component = wrapper.instance();
+  })
+
+  it('sets secrets from props', () => {
+    expect(component.props.secrets).toEqual(secrets)
+  })
+
+  it('Uploading state: correct content for div', () => {
+    wrapper.setState({ state: "loading" })
+    let componentText = wrapper.find('div').text();
+    expect(componentText).toEqual("Uploading the image... 10%")
+  })
+
+})
