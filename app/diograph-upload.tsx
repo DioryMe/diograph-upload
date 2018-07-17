769ee69852caee299e21565141ecea5162a7fe95
Jouni Alanen <jouni@diory.me>
2018-07-17 21:38:10 +0300

    UI states defined to render function: pending, loading, success, error

--- app/diograph-upload.tsx
+++ app/diograph-upload.tsx
@@ -1,22 +1,35 @@
 import * as React from 'react';
 import { Upload } from './lib/upload';
 
-export interface UploadState {}
+export interface UploadState { state: string }
 export interface UploadProps { secrets: any }
 
 export class DiographUpload extends React.Component <UploadProps, UploadState> {
 
   constructor(props) {
     super(props)
-    this.state = {searchResults: [], searchTerm: ""}
+    this.state = {state: "loading"}
   }
 
   render() {
-    return (
-      <div>
-        <input type="file" onChange={ event => Upload.uploadFiles(event, this.props.secrets.master) } />
-      </div>
-    )
+    switch(this.state.state) {
+      case "pending": {
+        return (
+          <div>
+            <input type="file" onChange={ event => Upload.uploadFiles(event, this.props.secrets.master) } />
+          </div>
+        )
+      }
+      case "loading": {
+        return ( <div>Uploading the image... 10%</div> )
+      }
+      case "success": {
+        return ( <div>Image succesfully uploaded!</div> )
+      }
+      case "error": {
+        return ( <div>Something went wrong while uploading the image...</div> )
+      }
+    }
   }
 
 }
