fe2dd8d87071d449d6075e164650b6ca3f560ad6
Jouni Alanen <jouni@diory.me>
2018-10-29 21:42:30 +0200

    webpack.config.js indentation fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,26 @@
 var webpack = require('webpack');
 
 module.exports = {
-    mode: "development",
-    entry: "./app/index.tsx",
-    output: {
-        filename: "./dist/bundle.js",
-        path: __dirname,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: [ { loader: "ts-loader" } ]
-        }
-      ]
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    plugins: [
-      new webpack.EnvironmentPlugin({
-        'DIOGRAPH_SERVER_HOST': 'http://localhost:3000'
-      })
+  mode: "development",
+  entry: "./app/index.tsx",
+  output: {
+    filename: "./dist/bundle.js",
+    path: __dirname,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [ { loader: "ts-loader" } ]
+      }
     ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      'DIOGRAPH_SERVER_HOST': 'http://localhost:3000'
+    })
+  ]
 };
-
